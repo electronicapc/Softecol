93864f7d7fbada5872ebae92a0ee59d642906696
electronicapc <electronicapcolombia@gmail.com>
2018-02-27 09:48:35 -0500

    Modificacion tamano logo
    Se agrego tamano logo para tablet

--- index.php
+++ index.php
@@ -343,18 +343,18 @@
         <script src="assets/js/scripts.js"></script>
         <script src="assets/js/odometer.js"></script>
         <!--Start of Tawk.to Script-->
-		<script type="text/javascript">
-			var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-			(function(){
-			var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-			s1.async=true;
-			s1.src='https://embed.tawk.to/592837c2b3d02e11ecc66fa0/default';
-			s1.charset='UTF-8';
-			s1.setAttribute('crossorigin','*');
-			s0.parentNode.insertBefore(s1,s0);
-			})();
-		</script>
-		<!--End of Tawk.to Script-->
+	<script type="text/javascript">
+		var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+		(function(){
+		var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+		s1.async=true;
+		s1.src='https://embed.tawk.to/592837c2b3d02e11ecc66fa0/default';
+		s1.charset='UTF-8';
+		s1.setAttribute('crossorigin','*');
+		s0.parentNode.insertBefore(s1,s0);
+		})();
+	</script>
+	<!--End of Tawk.to Script-->
     </body>
 </html>
 
